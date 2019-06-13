4713c951b6f27ef396bb546390b42208dc82c09e
Luís Eduardo <luiseduardogfranca@gmail.com>
2019-06-13 15:51:52 -0300

    Create cosine distance function

--- algorithms/python/utils/similarity_function.py
+++ algorithms/python/utils/similarity_function.py
@@ -5,3 +5,10 @@ def euclidian_dist(first_el, second_el):
 
 def manhattan_dist(first_el, second_el):
     return sum(abs(np.array(first_el) - np.array(second_el)))
+
+def cosine_dist(first_el, second_el):
+
+    def magnitude(arr):
+        return np.sqrt(np.sum(arr ** 2))
+
+    return np.sum(first_el * second_el) / (magnitude(first_el) * magnitude(second_el))
