c6253ba2d46ec030846131dc30d61553e26a59b0
SilvaEmerson <emersonjose877@gmail.com>
2019-10-03 15:44:23 -0300

    Add more tests

--- algorithms/Clojure/knn/test/knn/similarity_functions_test.clj
+++ algorithms/Clojure/knn/test/knn/similarity_functions_test.clj
@@ -2,9 +2,9 @@
   (:require [clojure.test :refer :all]
             [knn.similarity-functions :refer :all]))
 
-(def first-instance '(1 2 3))
-(def second-instance '(1 2 3))
+(def first-instance [1 2 3])
+(def second-instance [1 2 3])
 
 (deftest euclidean-distance-tests
   (testing "should return distance equals 0"
-  (is (= 0.0 (euclidean-distance first-instance second-instance)))))
+    (is (= 0.0 (euclidean-distance first-instance second-instance)))))
